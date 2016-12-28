0ab83d1ae5e985dd6009a66a3ea99359f462570a
ktaotao <k-toyoda@historia.co.jp>
2016-12-28 17:18:16 +0900

    CaptureScreenShot for ４．１３を追加。

---
+++ Plugins/CaptureScreenShot/Source/CaptureScreenShot/Private/CaptureScreenshotFunctionLibrary.cpp
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CaptureScreenShotPrivatePCH.h"
+#include "CaptureScreenshotFunctionLibrary.h"
+
+
+
+void UCaptureScreenshotFunctionLibrary::CaptureScreenshotWithHUD(const FString & InFilename, bool bInShowUI)
+{
+	FScreenshotRequest SR = FScreenshotRequest();
+	FString savelocation = FPaths::ConvertRelativePathToFull(FPaths::GameDir());
+
+	FString filename = savelocation + FString(TEXT("/Saved/Screenshots/")) + InFilename + FString(TEXT(".png"));
+
+	SR.RequestScreenshot(filename, bInShowUI, false);
+}
---
+++ Plugins/CaptureScreenShot/Source/CaptureScreenShot/Public/CaptureScreenshotFunctionLibrary.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "CaptureScreenshotFunctionLibrary.generated.h"
+
+UCLASS()
+class CAPTURESCREENSHOT_API UCaptureScreenshotFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	/**
+	 HUD込みで、スクリーンショットをとります。
+	 @param InFilename     画像の保存名を記入します。
+	 @param bInShowUI      HUD込み機能オン/オフをチェックします。
+	 */
+	UFUNCTION(BlueprintCallable, Category = "ScreenShot")
+	static void CaptureScreenshotWithHUD(const FString & InFilename=FString(TEXT("ImageName")), bool bInShowUI=true);
+
+};
